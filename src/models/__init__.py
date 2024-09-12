58c5f1a428b0e2a7e908b105807cdfad1f56ddb5
paolo-asus <paol.palumbo@gmail.com>
2024-09-12 23:41:24 +0200

    Refactor imports in __init__.py to handle missing modules gracefully

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,5 +1,8 @@
 from .forest_model import RandomForestModel
-from .nn_model import NeuralNetworkModel
 from .tree_model import TreeModel
 from .regressor_tree_model import RegressorTreeModel
-from .regressor_nn_model import RegressorNeuralNetworkModel
+try:
+    from .nn_model import NeuralNetworkModel
+    from .regressor_nn_model import RegressorNeuralNetworkModel
+except ImportError:
+    pass
